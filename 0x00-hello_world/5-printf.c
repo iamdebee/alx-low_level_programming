4380bf7fd9182c1fe06001561ebeb7c9bc7cdc2f
iamdebee <danoemilio2003@gmail.com>
2022-09-08 22:23:20 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main-print string
- * 
+ * main - print string
  * Return: always 0
-*/
+ */
 int main(void)
 {
-	printf("with the proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
-		
