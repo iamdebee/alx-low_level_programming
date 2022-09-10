cdf30c4be0c4f4f2915ee1b6b1b7d4bc7bbacf56
iamdebee <danoemilio2003@gmail.com>
2022-09-09 18:53:35 -0700

    Print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int n = 1;
+	int d1;
+	int d2;
+
+	for (d1 = 0; d1 <= 9; d1++) {
+		for (d2 = n; d2 <= 9; d2++) {
+			putchar(d1 + '0');
+			putchar(d2 + '0');
+
+			if (d1 == 9 && d2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return 0;
+}
