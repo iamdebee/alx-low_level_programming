6c805b95056bd753738ec81e2cf111e6dac4a3f8
iamdebee <danoemilio2003@gmail.com>
2022-09-09 20:48:06 -0700

    2 Print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int a;
+
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			putchar(a);
+		}
+		putchar('\n');
+		return (0);
+}
