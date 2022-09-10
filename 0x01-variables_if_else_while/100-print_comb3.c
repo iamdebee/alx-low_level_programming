f27e45f160152fbc32a4f33f44cf55ef83072695
iamdebee <danoemilio2003@gmail.com>
2022-09-10 14:50:16 -0700

    10 task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,7 +5,7 @@ int main()
 	int tens, unit, k;
 
 	k = '1';
-	for (tens = '0''; tens <= '8'; tens++)
+	for (tens = '0'; tens <= '8'; tens++)
 	{
 		for (unit = 'k'; unit <= '9'; units++)
 		{
@@ -18,5 +18,6 @@ int main()
 		}
 		k++;
 	}
+	putchar('\n');
 	return (0);
 }
