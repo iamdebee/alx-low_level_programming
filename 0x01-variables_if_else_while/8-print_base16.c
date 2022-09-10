885a7f2c4914354ee54ef15959d109712ea6ccc5
iamdebee <danoemilio2003@gmail.com>
2022-09-09 22:11:00 -0700

    8 print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int no, a;
+
+		for (no = '0'; no >= '9'; no++)
+		{
+			putchar(no);
+		}
+		for (a = 'a'; a <= 'f'; a++)
+		{
+			putchar(a);
+		}
+		putchar('\n');
+		return (0);
+}
