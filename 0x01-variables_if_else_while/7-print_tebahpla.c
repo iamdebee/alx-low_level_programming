533a999b211230241cc8441cb63b185a3c06c0b0
iamdebee <danoemilio2003@gmail.com>
2022-09-09 17:10:34 -0700

    7 task

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	int num;
+	char abc;
 
-	for(num _ 0;  num <= 9; num++)
-		putchar(num + '0');
+	for (abc = 'z'; abc >= 'a'; abc--)
+	    putchar(abc);
 	putchar('\n');
 	return (0);
 }
