751fe45aaa34ef767d4fd47050467e70aedef495
iamdebee <danoemilio2003@gmail.com>
2022-09-11 02:08:27 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,29 +6,23 @@
  */
 int main(void)
 {
-	int huns, tens, units, i, j;
-	int _i, _j;
-	i = _i = '1';
-	j = _j = '2';
-	for (huns = '0'; huns <= '9'; huns++)
+	int i, j;
+	for (i = 0; i <= 9; i++)
 	{
-		for (tens = i; tens <= '9'; tens++)
+		for (j = 1; j <= 9; j++)
 		{
-			for (units = j; units <= '9'; units++)
+			if (j > i)
 			{
-				putchar(huns);
-				putchar(tens);
-				putchar(units);
-				if (huns == '7' && tens == '8' && units == '9')
-					break;
-				putchar(',');
-				putchar(' ');
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			j++;
 		}
-		i++;
-		_j++;
-		j = _j;
-	}	
+	}
+	putchar('\n');
 	return (0);
 }
