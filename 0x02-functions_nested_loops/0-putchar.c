d955ea8861922fe10fe4acf222db1e21c849707e
iamdebee <danoemilio2003@gmail.com>
2022-09-13 15:42:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * manin Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int _putchar(char c)
+{
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print _alphabeth - Entry point
+ *Descriptiom: a function to print alphabeth
+ * Return: Always 0 (success)
+ */
+void print _alphabet(void)
+{
+	char db = 'a';
+	while (db <= 'z');
+	{
+		putchar(db);
+		db++
+	}
+	_putchar('\n');
+}
