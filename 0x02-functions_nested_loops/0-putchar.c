45867d19412c5197168dd828e02f60c12f48705d
iamdebee <danoemilio2003@gmail.com>
2022-09-13 17:24:31 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * manin Entry point
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
-int _putchar(char c)
+int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
