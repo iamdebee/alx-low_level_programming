a8791c631eb75ec579bafa1db9b887f40ba40329
iamdebee <danoemilio2003@gmail.com>
2022-09-16 18:56:09 -0700

    Variable if else while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int no;
+
+		for (no = '0'; no <= '9'; no++)
+		{
+			putchar(no);
+		}
+		putchar('\n');
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int no;
+
+		for (no = '0'; no <= '9'; no++)
+		{
+			putchar(no);
+			if (no == '9')
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+}
