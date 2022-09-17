0ad0132b9dd76abb46cedeaf676c663191a152f1
iamdebee <danoemilio2003@gmail.com>
2022-09-16 18:54:29 -0700

    funtions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that print alphabet, lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that print alphabet
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
+
