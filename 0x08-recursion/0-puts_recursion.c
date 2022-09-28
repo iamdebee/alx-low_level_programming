06260adcea0f516def41ded173eec00cfe31c2ec
iamdebee <danoemilio2003@gmail.com>
2022-09-28 02:26:40 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+include "main.h"
+
+/** Main - Write a function that prints a string, followed by a new line.
+ *
+ * Return 0 Always
+ */
+int main(void)
+{
+	char s[] = "this is the way.";
+
+	return 0;
+}
